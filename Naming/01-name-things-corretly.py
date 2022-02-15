d6975ccf67769da0adf9bd2609fb0be472c21a31
mahdi <mahdi100th@gmail.com>
2022-02-15 20:40:34 +0430

    Started Clean code course

---
+++ Naming/01-name-things-corretly.py
@@ -0,0 +1,10 @@
+# How to name things correctly: 
+    # Variables and constants: (Data containers) 
+        # - use nouns or short phrases with adjectives
+        # Example:  user_data, is_valid, is_valid_user, is_valid_password
+    # Functions/Methods: (Commands or calculated values)
+        # - use verbs or short phrases with adjectives
+        # Example: send_data, input_is_valid, get_data, get_user_data, get_password_data 
+    # Classes: (Use classes to create things)
+        # - use nouns or short phrases with nouns
+        # Example: User, 
